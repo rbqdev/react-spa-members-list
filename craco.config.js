7e4f642a9e45deffbb26dd1fcfb4d30ca46fd21f
rbqdev <rbq.dev@gmail.com>
2024-02-12 11:19:40 -0300

    chore: more configs -> craco + shadcn + jsm color token update

--- craco.config.js
+++ craco.config.js
@@ -5,6 +5,8 @@ module.exports = {
     alias: {
       "@components": path.resolve(__dirname, "./src/components"),
       "@pages": path.resolve(__dirname, "./src/pages"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
     },
   },
 };
